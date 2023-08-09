0c3dca05b2f56e1d2c6fa39903379acdcbebbb6d
auom.eisiad <daisiemouaa@gmail.com>
2023-08-09 17:32:14 -0400

    added comments to script

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
 //Assignment code here
-// function generatePassword() {
+// function genPassword() {
 // var inputPassword = document.getElementById("password");
 // var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+// var lengthPassword = 12;
 // var input = "";
 
-// for (var i = 0; i <= inputPassword.length; i++) {
+// for (var i = 0; i <= lengthPassword; i++) {
 //     var randomNumber = Math.floor(Math.random() * characters.length);
 //     inputPassword += characters.substring(randomNumber, randomNumber + 1);
 //     }  
 //         document.getElementById("password").value = password;
 // }
 
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
+// genPassword();
+
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
 
 
 // WHEN prompted for password criteria
 // THEN I select which criteria to include in the password
 
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-
-//make sure user only put at least 8 characters and no more than 128 characters.
-// the prompt will error if variable length is less than 8 characters or greater than 128.
-
+//funtion to validate the length of the password
 function validateLength() {
+    // prompt user to choose how long they want their password
     var length = window.prompt("Please enter the length of your password (need to be 8 and no more than 128 characters.)");
     
+    // condition the length to where the computer will read "length" as an integar
     if ( parseInt(length)) {
+
+        // holds the conversion in a variable
         var lengthInt = parseInt(length);
+
+        // if the user input a length les than 8 or greater than 128, the prompt will alert them.
         if (lengthInt < 8 || lengthInt > 128) {
             window.alert("Password must be between 8 to 128 characters.");
+
+            // function calls itself back
             validateLength();
         }
     }
     else {
+        // if user input something other than a number, the prompt will alert them
         window.alert("Password length must be a number");
+
+        //function will call itself back
         validateLength();
     }
 }
@@ -53,7 +62,9 @@ var specialChar = window.confirm("Is there any special characters?")
 // WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected
 
-
+function genPassword() {
+    if ()
+}
 
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
